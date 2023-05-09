c6592f74ec2ac2df824627ecf3b92467c3f6eb3d
muna <munapm1@gmail.com>
2023-05-09 22:36:47 +0530

    carousel

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        navText:["PREV","NEXT"],
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+});
